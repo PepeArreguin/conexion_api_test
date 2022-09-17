fde5b84f6e0e56a64fe4f6a1ba2cf4e904e9be6b
PepeArreguin <arreguinpepe@gmail.com>
2022-09-17 10:40:45 -0500

    codigo refactorizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log("Entro al main.js");
+
+const imgRandom = document.getElementById('imgRandom');
+const URL = "https://picsum.photos/200/300";
+
+const randomImage = () => {
+    console.log("entro a la funcion randomImage");
+    //Es una promesa
+    fetch(URL)
+        .then((data) => {
+            console.log(data);
+            imgRandom.src = data.url;
+            })
+        .catch((error) => {
+            console.log("error");
+            });
+
+}
